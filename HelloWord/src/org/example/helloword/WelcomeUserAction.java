07d98d929cfc93a2f0e84deb229bf9f6c38ef182
Viresh Managooli <viresh.managooli@gmail.com>
2014-05-10 22:43:37 +0530

    First Version

---
+++ HelloWord/src/org/example/helloword/WelcomeUserAction.java
@@ -0,0 +1,21 @@
+package org.example.helloword;
+
+public class WelcomeUserAction{
+
+	private String username;
+	 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// all struts logic here
+	public String execute() {
+
+		return "SUCCESS";
+
+	}
+}
